7b568d8a23ce5ca8e1b8f4eb6154483713899da3
shub <cptnshubham39@gmail.com>
2024-01-21 09:56:04 +0530

    track the number of sins you commit

--- app/src/main/java/com/example/veiwsapp/MainActivity.kt
+++ app/src/main/java/com/example/veiwsapp/MainActivity.kt
@@ -4,26 +4,47 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val name : EditText = findViewById(R.id.editTextText)
-        val btn = findViewById<Button>(R.id.button)
+        val name : EditText = findViewById(R.id.namebox)
+        val btn = findViewById<Button>(R.id.toastbtn)
+        val sins : TextView = findViewById(R.id.textView3)
+        val btn2 = findViewById<Button>(R.id.button2)
 
-
-        btn.setOnClickListener(){
+        btn.setOnClickListener() {
             val text = name.text
+            val sons = sins.text
 
-            Toast.makeText(this,
-                "Hello "+ text,
-                Toast.LENGTH_LONG).show()
+            Toast.makeText(
+                this,
+                "Hello " + text + ", You have committed " + sons + " Sins.",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+        btn2.setOnClickListener(){
+            sins.setText(""+increasecounter())
 
         }
+
     }
+
+    fun increasecounter(): Int{
+        counter++
+        return counter
+
+    }
+
+
 }
 
 
