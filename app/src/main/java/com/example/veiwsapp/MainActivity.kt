41faf45aa56e746464fea66a49c2621b91f44404
shub <cptnshubham39@gmail.com>
2024-01-21 09:26:27 +0530

    Simple toast app

--- app/src/main/java/com/example/veiwsapp/MainActivity.kt
+++ app/src/main/java/com/example/veiwsapp/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.veiwsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val name : EditText = findViewById(R.id.editTextText)
+        val btn = findViewById<Button>(R.id.button)
+
+
+        btn.setOnClickListener(){
+            var text = name.text
+
+            Toast.makeText(this,
+                "Hello "+ text,
+                Toast.LENGTH_LONG).show()
+
+        }
     }
 }
+
+
