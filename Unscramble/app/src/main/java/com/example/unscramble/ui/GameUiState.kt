5c03ad9792a0167f633c7dd77a51560b65c69dc5
shub39 <cptnshubham39@gmail.com>
2024-05-23 11:28:41 +0530

    Unscramble game

---
+++ Unscramble/app/src/main/java/com/example/unscramble/ui/GameUiState.kt
@@ -0,0 +1,11 @@
+package com.example.unscramble.ui
+
+import androidx.compose.runtime.mutableStateOf
+
+data class GameUiState(
+    val currentScrambledWord: String = "",
+    val currentWordCount: Int = 1,
+    val isGuessedWordWrong: Boolean = false,
+    val score: Int = 0,
+    val isGameOver: Boolean = false
+)
